472c81920bfdccf03500aff4b2e6bd022c0a45bd
nonniii <113233864+nonniii@users.noreply.github.com>
2024-02-18 16:56:38 +0100

    Initial commit

---
+++ src/main/java/com/example/obli1data17002024/Obli1Data17002024Application.java
@@ -0,0 +1,13 @@
+package com.example.obli1data17002024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Obli1Data17002024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Obli1Data17002024Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/obli1data17002024/Obli1Data17002024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obli1data17002024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Obli1Data17002024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
